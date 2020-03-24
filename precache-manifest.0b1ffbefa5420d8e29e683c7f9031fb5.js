a636ee3e087bb6d0aa5f6ad03a23addec0a01991
Joenele <joenele.young@gmail.com>
2020-03-24 17:27:05 -0400

    Updates

---
+++ precache-manifest.0b1ffbefa5420d8e29e683c7f9031fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0310b88c69522c31df49cf334883a3c9",
+    "url": "/mapmarker-react-app/index.html"
+  },
+  {
+    "revision": "d9d950a89c59674eed96",
+    "url": "/mapmarker-react-app/static/css/main.dc4f3579.chunk.css"
+  },
+  {
+    "revision": "cf03325bdcf451979367",
+    "url": "/mapmarker-react-app/static/js/2.f04f4bcf.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/mapmarker-react-app/static/js/2.f04f4bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d950a89c59674eed96",
+    "url": "/mapmarker-react-app/static/js/main.9c0b045d.chunk.js"
+  },
+  {
+    "revision": "e8958f22c906a2e16e01",
+    "url": "/mapmarker-react-app/static/js/runtime-main.55874944.js"
+  }
+]);
